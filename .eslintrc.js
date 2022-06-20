d1b85c21e534b3dbb574fc1b6025af94bcf412e2
yanachuwan9sm <gurem919@icloud.com>
2022-06-20 19:24:44 +0900

    -config : ⚡ SlackBotにおける動作確認完了

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,18 +9,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     // prettierのextendsは他のextendsより後に記述する
     'prettier',
-    // 2021-02-21から記述不要
-    // "prettier/@typescript-eslint",
   ],
   plugins: ['@typescript-eslint'],
-  // ESLintでTypescriptを解析する
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
-    // TypeScriptのLint時に参照するconfigファイルを指定
     project: './tsconfig.json',
   },
-  // 上位ディレクトリにある親のeslintrcを参照しないようにする
   root: true,
   rules: {},
 };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,37 @@
-export default class Test {
-  private message: string;
+import { App, ExpressReceiver } from '@slack/bolt';
+import { ExtendedErrorHandlerArgs } from '@slack/bolt/dist/App';
 
-  constructor(message: string) {
-    this.message = message;
-  }
+const singingSecret = process.env.SLACK_SIGNING_SECRET as string;
+const slackToken = process.env.SLACK_BOT_TOKEN;
 
-  public sayHello(element: HTMLElement | null) {
-    if (element) {
-      element.innerText = this.message;
-    }
-  }
-}
+// App initialization
+// memo : Bolt内部の Express を明示的に設定する
+// SlackイベントだけでなくHTTPリクエストを受け取る
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET as string,
+});
+
+const config = {
+  receiver,
+  signingSecret: singingSecret,
+  token: slackToken,
+};
+
+const app = new App(config);
+
+app.error(async ({ error, logger }: ExtendedErrorHandlerArgs) => {
+  logger.error(error);
+});
+
+app.event('app_home_opened', async ({ event, say }) => {
+  say(`Hello world, <@${event.user}>!`);
+});
+
+// Start
+(async () => {
+  // Start the app
+  const port = process.env.PORT || 3000;
+  await app.start(port as number);
+
+  console.log(`listening on port ${port}!`);
+})();
